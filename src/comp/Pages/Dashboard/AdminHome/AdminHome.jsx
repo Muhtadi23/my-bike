d3a3264746bbda764868f6dc7b69ad6cd441b595
Muhtadi23 <miranmuhtadi121@gmail.com>
2024-08-26 02:01:23 +0600

    all done

--- src/comp/Pages/Dashboard/AdminHome/AdminHome.jsx
+++ src/comp/Pages/Dashboard/AdminHome/AdminHome.jsx
@@ -24,46 +24,6 @@ const AdminHome = () => {
                         user?.displayName ? user.displayName : 'Back'
                     }
                 </h2>
-                {/* <div className="stats shadow">
-
-                    <div className="stat">
-                        <div className="stat-figure text-secondary">
-                            <FaDollarSign className='text-3xl'></FaDollarSign>
-                        </div>
-                        <div className="stat-title">Revenue</div>
-                        <div className="stat-value">$</div>
-                        <div className="stat-desc">Jan 1st - Feb 1st</div>
-                    </div>
-
-                    <div className="stat">
-                        <div className="stat-figure text-secondary">
-                            <FaUsers className='text-3xl'></FaUsers>
-                        </div>
-                        <div className="stat-title">Users</div>
-                        <div className="stat-value"></div>
-                        <div className="stat-desc">↗︎ 400 (22%)</div>
-                    </div>
-
-
-                    <div className="stat">
-                        <div className="stat-figure text-secondary">
-                            <FaBook className='text-3xl'></FaBook>
-                        </div>
-                        <div className="stat-title">Menu Items</div>
-                        <div className="stat-value"></div>
-                        <div className="stat-desc">↗︎ 400 (22%)</div>
-                    </div>
-
-                    <div className="stat">
-                        <div className="stat-figure text-secondary">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-8 h-8 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 8h14M5 8a2 2 0 110-4h14a2 2 0 110 4M5 8v10a2 2 0 002 2h10a2 2 0 002-2V8m-9 4h4"></path></svg>
-                        </div>
-                        <div className="stat-title">Orders</div>
-                        <div className="stat-value"></div>
-                        <div className="stat-desc">↘︎ 90 (14%)</div>
-                    </div>
-
-                </div> */}
                 <div className="mx-auto max-w-screen-xl px-4 sm:px-6 sm:py-12 lg:px-8">
                     <dl className="grid grid-cols-1 gap-4 sm:mt-8 sm:grid-cols-2 lg:grid-cols-4">
                         <div className="flex flex-col rounded-lg px-4 py-8 text-center">
--- src/comp/Social/Social.jsx
+++ src/comp/Social/Social.jsx
@@ -2,6 +2,63 @@ const Social = () => {
     return (
         <div>
             <h1 className=" text-5xl text-center">Upcoming Events</h1>
+
+            <div className="overflow-x-auto">
+                <table className="table table-zebra">
+
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Event Name</th>
+                            <th>Date</th>
+                            <th>Location</th>
+                            <th><button className="btn btn-ghost">Register</button></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+
+                        <tr>
+                            <th>1</th>
+                            <td>Tour de France</td>
+                            <td>July 1-23, 2024</td>
+                            <td>France</td>
+                            <th><button className="btn btn-ghost">Register</button></th>
+                        </tr>
+
+                        <tr>
+                            <th>2</th>
+                            <td>Boston Marathon</td>
+                            <td>April 15, 2024</td>
+                            <td>Boston, MA, USA</td>
+                            <th><button className="btn btn-ghost">Register</button></th>
+                        </tr>
+
+                        <tr>
+                            <th>3</th>
+                            <td>Ironman World Championship</td>
+                            <td>October 12, 2024</td>
+                            <td>Kailua-Kona, HI, USA</td>
+                            <th><button className="btn btn-ghost">Register</button></th>
+                        </tr>
+
+                        <tr>
+                            <td>4</td>
+                            <td>Giro d'Italia</td>
+                            <td>May 4-26, 2024</td>
+                            <td>Italy</td>
+                            <th><button className="btn btn-ghost">Register</button></th>
+                        </tr>
+                        <tr>
+                            <td>4</td>
+                            <td>New York City Marathon</td>
+                            <td>November 3, 2024</td>
+                            <td>New York, NY, USA
+                            </td>
+                            <th><button className="btn btn-ghost">Register</button></th>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
